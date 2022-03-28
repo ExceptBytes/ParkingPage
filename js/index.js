5ba799e61a9456f33e526fe1e24b6ee7670ad802
Mark Heijnekamp <mark@heijnekamp.nl>
2022-03-28 15:10:59 +0200

    Added: - UpdateHead for better SEO score. - Dynamic title. - Global object with all the keywords and values Updated: - Current year is now gathered with javascript. See @getYear

--- js/index.js
+++ js/index.js
@@ -1,10 +1,37 @@
+const variables = {
+  "year": getYear(),
+  "site": window.location.hostname,
+};
+
+function updateKeyword(s){
+  if(s == null || s == undefined){
+    return s;
+  }
+  Object.keys(variables).forEach(function(k){
+  s = s.replace(`{${k}}`,variables[k].toString());
+});
+return s;
+}
 function getYear() {
-    return 2022;
+    return new Date().getFullYear();
 }
 function updateYear() {
-    let c = document.getElementById("copyright");
-    c.innerHTML = c.innerHTML.replace("{Year}", getYear().toString());
+    var c = document.getElementById("copyright");
+    c.innerHTML = updateKeyword(c.innerHTML).toString();
 }
+
+function updateHead(){
+    var headElements = document.head.childNodes;
+    headElements.forEach(element => {
+      element.content = updateKeyword(element.content);
+    });
+    updateTitle();
+}
+function updateTitle(){
+  document.title = updateKeyword(document.title);
+}
+
 
 
 updateYear();
+updateHead();
